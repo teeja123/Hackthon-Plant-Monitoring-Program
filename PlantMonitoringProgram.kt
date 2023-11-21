51b3ad0dfc43f1d85070138a0af19ab9342cc89c
teeja123 <151339113+teeja123@users.noreply.github.com>
2023-11-21 17:40:11 +0400

    Update PlantMonitoringProgram.kt

--- PlantMonitoringProgram.kt
+++ PlantMonitoringProgram.kt
@@ -1,3 +1,4 @@
+
 package com.example.myapplication
 
 import android.os.Bundle
@@ -12,8 +13,7 @@ import com.google.cloud.language.v1.LanguageServiceClient
 import org.w3c.dom.Text
 import com.google.auth.oauth2.GoogleCredentials
 import java.io.FileInputStream
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+
 import kotlinx.coroutines.launch
 
 data class Plant(val name: String, val growthRate: Double, val sensoryReceptors: List<SensoryReceptor>)
@@ -24,35 +24,32 @@ data class SensoryReceptor(val id: String, val type: String)
 class MainActivity() : ComponentActivity() {
 
 
-    private lateinit var comparisonTextView:TextView
-    private lateinit var  sentimentTextView1:TextView
-    private lateinit var sentimentTextView2:TextView
-    private lateinit var plant1DataTextView:TextView
-    private lateinit var plant2DataTextView:TextView
-
-
-
+    private lateinit var plant1DataTextView: TextView
+    private lateinit var plant2DataTextView: TextView
+    private lateinit var comparisonTextView: TextView
+    private lateinit var sentimentTextView1: TextView
+    private lateinit var sentimentTextView2: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val serviceAccountKeyPath = "path/to/your/service_account_key.json"
-        val credentials = GoogleCredentials.fromStream(FileInputStream(serviceAccountKeyPath))
 
         // Create a LanguageServiceClient using the loaded credentials
         setContentView(R.layout.layout)
 
+
+
         // Create Plant1
         val plant1 = Plant(
-            "Sunflower",
+            "Sunflower1",
             0.75,
             listOf(SensoryReceptor("1", "type1"), SensoryReceptor("2", "type2"))
         )
 
         // Create Plant2 with texture
         val plant2 = Plant(
-            "Orchids",
+            "Sunflower2",
             0.85,
             listOf(
                 SensoryReceptor("3", "type1"),
@@ -60,52 +57,57 @@ class MainActivity() : ComponentActivity() {
                 SensoryReceptor("5", "texture")
             )
         )
-        // Compare Plant1 and Plant2
-        val comparisonResult = comparePlants(plant1, plant2)
-        GlobalScope.launch(Dispatchers.IO) {
-            // Simulate sentiment analysis for Plant1
-            val sentimentResult1 = analyzeSentiment(plant1)
 
-            // Simulate sentiment analysis for Plant2
-            val sentimentResult2 = analyzeSentiment(plant2)
-            launch(Dispatchers.Main) {
-                // Display results in TextViews
-                val comparisonTextView: TextView = findViewById(R.id.comparisonTextView)
-                comparisonTextView.text = comparisonResult
 
-                val sentimentTextView1: TextView = findViewById(R.id.sentimentTextView1)
-                sentimentTextView1.text = sentimentResult1
+        val plant1DataTextView: TextView = findViewById(R.id.plant1DataTextView)
+        plant1DataTextView.text =
+            "Plant1 Data:\nName: ${plant1.name}\nGrowth Rate: ${plant1.growthRate}"
 
-                val sentimentTextView2: TextView = findViewById(R.id.sentimentTextView2)
-                sentimentTextView2.text = sentimentResult2
+        val plant2DataTextView: TextView = findViewById(R.id.plant2DataTextView)
+        plant2DataTextView.text =
+            "Plant2 Data:\nName: ${plant2.name}\nGrowth Rate: ${plant2.growthRate}"
 
-                val plant1DataTextView: TextView = findViewById(R.id.plant1DataTextView)
-                plant1DataTextView.text =
-                    "Plant1 Data:\nName: ${plant1.name}\nGrowth Rate: ${plant1.growthRate}"
+        val comparisonTextView: TextView = findViewById(R.id.comparisonTextView)
+        comparisonTextView.text = comparePlants(plant1, plant2)
 
-                val plant2DataTextView: TextView = findViewById(R.id.plant2DataTextView)
-                plant2DataTextView.text =
-                    "Plant2 Data:\nName: ${plant2.name}\nGrowth Rate: ${plant2.growthRate}"
-            }
-        }
+        val sentimentTextView1: TextView = findViewById(R.id.sentimentTextView1)
+        sentimentTextView1.text = analyzeSentiment(plant1)
+
+        val sentimentTextView2: TextView = findViewById(R.id.sentimentTextView2)
+        sentimentTextView2.text = analyzeSentiment(plant2)
+
+        // Display results in TextViews
+
+
     }
-
 
 
     // Function to compare two plants based on growth rate
     private fun comparePlants(plant1: Plant, plant2: Plant): String {
+
         return when {
             plant1.growthRate > plant2.growthRate -> "${plant1.name} has a higher growth rate."
             plant1.growthRate < plant2.growthRate -> "${plant2.name} has a higher growth rate."
             else -> "Both plants have the same growth rate."
+
+
         }
+
+
     }
+    private fun createDocument(plant: Plant): Document {
+        return Document.newBuilder()
+            .setContent("${plant.name}'s energy levels are positive.")
+            .setType(Document.Type.PLAIN_TEXT)
+            .build()
+    }
+
 
     // Function to simulate sentiment analysis
     private fun analyzeSentiment(plant: Plant): String {
         try {
             val languageServiceClient = LanguageServiceClient.create()
-            val document = Document.newBuilder().setContent("${plant.name}'s energy levels are positive.").setType(Document.Type.PLAIN_TEXT).build()
+            val document = createDocument(plant)
             val sentiment = languageServiceClient.analyzeSentiment(document).documentSentiment.score
             languageServiceClient.close()
 
@@ -119,8 +121,6 @@ class MainActivity() : ComponentActivity() {
             return "Error analyzing sentiment."
         }        }
 }
-
-
 
 
 
